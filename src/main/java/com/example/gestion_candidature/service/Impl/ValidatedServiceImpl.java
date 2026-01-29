1edf1886c987a165431ac4227948b525b01ac390
omarzandag <omar.zandag@gmail.com>
2026-01-29 16:14:36 +0100

    add service files

---
+++ src/main/java/com/example/gestion_candidature/service/Impl/ValidatedServiceImpl.java
@@ -0,0 +1,4 @@
+package com.example.gestion_candidature.service.Impl;
+
+public class ValidatedServiceImpl {
+}
---
+++ src/main/java/com/example/gestion_candidature/service/facade/ValidateService.java
@@ -0,0 +1,4 @@
+package com.example.gestion_candidature.service.facade;
+
+public interface ValidateService {
+}
