ea5fd5f7df9298c8fc2f59f8f0e311d3dfc0e873
omarzandag <omar.zandag@gmail.com>
2026-01-29 15:50:56 +0100

    push first files existing

---
+++ src/main/java/com/example/gestion_candidature/GestionCandidatureApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestion_candidature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCandidatureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionCandidatureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gestion_candidature/GestionCandidatureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestion_candidature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCandidatureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
